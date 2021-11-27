b8dd7ec92985835a35905bac900fee561b789f64
Logan Stucki <loganstucki@gmail.com>
2021-11-26 22:37:17 -0700

    move footer and header to their own folder

--- App.tsx
+++ App.tsx
@@ -12,8 +12,8 @@ import ExerciseLog from "./screens/ExerciseLog";
 import AddSet from "./screens/AddSet";
 import EditSet from "./screens/EditSet";
 import ExerciseSetsProvider from "./data/exerciseSetsProvider";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import { en, registerTranslation } from "react-native-paper-dates";
 registerTranslation("en", en);
